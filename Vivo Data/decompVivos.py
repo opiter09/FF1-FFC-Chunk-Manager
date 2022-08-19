da30d354e93e54c920d958c4164ea7662162a95b
opiter09 <92452568+opiter09@users.noreply.github.com>
2022-08-19 18:00:17 -0400

    can change sizes

--- Vivo Data/decompVivos.py
+++ Vivo Data/decompVivos.py
@@ -1,5 +1,10 @@
+import os
+
 binn = open("combined.bin", "rb")
-section = binn.read(1072)
+whole = binn.read()
+binn.close()
+binn = open("combined.bin", "rb")
+section = binn.read(48)
 binn.close()
 
 binn = open("combined.bin", "wb")
@@ -8,6 +13,11 @@ binn.close()
 
 binn = open("combined.bin", "ab")
 text = open("vivoNames.txt", "rt").read()
+offset = int.from_bytes(whole[44:48], "little")
+for i in range(209):
+    offset = offset + os.stat("vivoFiles/" + text.split("\n")[i] + "_" + str(i) + ".bin").st_size
+    binn.write(offset.to_bytes(4, "little"))
+binn.write(whole[884:1072])
 for i in range(210):
     binn.write(open("vivoFiles/" + text.split("\n")[i] + "_" + str(i) + ".bin", "rb").read())
 binn.close()
@@ -19,4 +29,4 @@ open("_Decompressed_00001AF4.bin", "wb").write(binn[16384:24576])
 open("_Decompressed_00002808.bin", "wb").write(binn[24576:32768])
 open("_Decompressed_00003714.bin", "wb").write(binn[32768:40960])
 open("_Decompressed_0000466C.bin", "wb").write(binn[40960:49152])
-open("_Decompressed_000054F4.bin", "wb").write(binn[49152:53916])
+open("_Decompressed_000054F4.bin", "wb").write(binn[49152:os.stat("combined.bin").st_size])

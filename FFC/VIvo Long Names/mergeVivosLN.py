a3d03947149a3be09e66c91d5f1beace04fae5c3
opiter09 <92452568+opiter09@users.noreply.github.com>
2022-11-03 20:09:33 -0400

    added vivo names

---
+++ FFC/VIvo Long Names/mergeVivosLN.py
@@ -0,0 +1,23 @@
+import os
+
+binn = open("0.bin", "rb")
+whole = binn.read()
+binn.close()
+binn = open("0.bin", "rb")
+section = binn.read(20)
+binn.close()
+
+binn = open("0.bin", "wb")
+binn.write(section)
+binn.close()
+
+binn = open("0.bin", "ab")
+text = open("vivoNamesL.txt", "rt").read()
+offset = int.from_bytes(whole[16:20], "little")
+for i in range(203):
+    offset = offset + os.stat("vivoLongNameFiles/" + text.split("\n")[i] + "_" + str(i + 1) + ".bin").st_size
+    binn.write(offset.to_bytes(4, "little"))
+binn.write((0x4E6F6E6500).to_bytes(5, "big"))
+for i in range(204):
+    binn.write(open("vivoLongNameFiles/" + text.split("\n")[i] + "_" + str(i + 1) + ".bin", "rb").read())
+binn.close()
---
+++ FFC/VIvo Long Names/splitVivosLN.bat
@@ -0,0 +1,2 @@
+cd ./
+python splitVivosLN.py

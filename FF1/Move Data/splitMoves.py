83048d4388a55ac2dafe75fa61b29de6b7ee45fe
opiter09 <92452568+opiter09@users.noreply.github.com>
2022-11-05 11:28:19 -0400

    added unused moves

--- FF1/Move Data/splitMoves.py
+++ FF1/Move Data/splitMoves.py
@@ -1,6 +1,6 @@
 binn2 = open("0.bin", "rb")
 bytE = binn2.read()
-text = open("ff1_names.txt", "rb").read().decode("UTF-8", errors = "ignore")
+text = open("moveNames.txt", "rb").read().decode("UTF-8", errors = "ignore")
 
 import os
 try:
@@ -8,11 +8,13 @@ try:
 except OSError as error:
     pass
         
-for i in range(408):
+for i in range(500):
     oldOffset = bytE[(24 + (i * 4)):(28 + (i * 4))]
     newOffset = bytE[(28 + (i * 4)):(32 + (i * 4))]
+    if (i == 499):
+        newOffset = (os.stat("0.bin").st_size).to_bytes(2, "little")
     
-    newFile = open("moveFiles/" + text.split("\n")[i] + "_" + str(i + 1) + ".bin", "wb")
+    newFile = open("moveFiles/" + (text.split("\n") + ([chr(0x9FAF) + "Unused"] * 92))[i] + "_" + str(i + 1) + ".bin", "wb")
     newFile.write(bytE[int.from_bytes(oldOffset, "little"):int.from_bytes(newOffset, "little")])
     newFile.close()
     

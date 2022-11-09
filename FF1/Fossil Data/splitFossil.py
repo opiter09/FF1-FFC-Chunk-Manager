63d3732eda16e2c8e01cfed6dc097ae3dee2eb69
opiter09 <92452568+opiter09@users.noreply.github.com>
2022-11-08 19:46:24 -0500

    added FF1 kaseki_defs

---
+++ FF1/Fossil Data/splitFossil.py
@@ -0,0 +1,31 @@
+import os
+import math
+try:
+    os.mkdir("kasekiFiles")
+except OSError as error:
+    pass
+
+file = open("0.bin", "rb")
+whole = file.read()
+file.close()
+
+text = open("vivoNames.txt", "rt").read()
+parts = [ "Head", "Body", "Arms", "Legs", "Other" ]
+count = -1
+for i in range(12, 0xFA5, 4):
+    count = count + 1
+    beg = int.from_bytes(whole[i:(i + 4)], "little")
+    end = int.from_bytes(whole[(i + 4):(i + 8)], "little")
+    if (i == 0xFA4):
+        end = os.stat("0.bin").st_size
+
+    if (count >= 1) and (count <= 400):
+        fileName = "./kasekiFiles/" + str(count).zfill(4) + "_" + text.split("\n")[math.floor((count - 1) / 4)] + "_" + parts[(count - 1) % 4] + ".bin"
+    else:
+        fileName = "./kasekiFiles/" + str(count).zfill(4) + ".bin"
+
+    new = open(fileName, "wb")
+    new.write(whole[beg:end])
+    new.close()
+
+    

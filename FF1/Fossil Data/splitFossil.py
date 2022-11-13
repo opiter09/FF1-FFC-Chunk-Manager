29a16927d2ee274881b17b21e607c6280fde6a4f
opiter09 <92452568+opiter09@users.noreply.github.com>
2022-11-13 13:40:34 -0500

    Update splitFossil.py

--- FF1/Fossil Data/splitFossil.py
+++ FF1/Fossil Data/splitFossil.py
@@ -11,7 +11,6 @@ file.close()
 
 text = open("vivoNames.txt", "rt").read()
 parts = [ "Head", "Body", "Arms", "Legs", "Other" ]
-chickens = { "425": "Squik", "429": "Squirk", "433": "Squirth", "437": "Squilk", "441": "Squiro" }
 count = -1
 for i in range(12, 0xFA5, 4):
     count = count + 1
@@ -20,10 +19,8 @@ for i in range(12, 0xFA5, 4):
     if (i == 0xFA4):
         end = os.stat("0.bin").st_size
 
-    if (count >= 1) and (count <= 400):
+    if (count >= 1) and (count <= 464):
         fileName = "./kasekiFiles/" + str(count).zfill(4) + "_" + text.split("\n")[math.floor((count - 1) / 4)] + "_" + parts[(count - 1) % 4] + ".bin"
-    elif (chickens.get(str(count)) != None):
-        fileName = "./kasekiFiles/" + str(count).zfill(4) + "_" + chickens[str(count)] + ".bin"
     else:
         fileName = "./kasekiFiles/" + str(count).zfill(4) + ".bin"
 

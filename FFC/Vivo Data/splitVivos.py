564378e3999e9f0f56b28b1473fac80c2dd61bfe
opiter09 <92452568+opiter09@users.noreply.github.com>
2023-06-07 17:03:42 -0400

    add range, fix harcoding

--- FFC/Vivo Data/splitVivos.py
+++ FFC/Vivo Data/splitVivos.py
@@ -12,8 +12,7 @@ for i in range(210):
     oldOffset = bytE[(44 + (i * 4)):(48 + (i * 4))]
     newOffset = bytE[(48 + (i * 4)):(52 + (i * 4))]
     if (i == 209):
-        newOffset = 53916
-        newOffset = newOffset.to_bytes(4, "little")
+        newOffset = len(bytE).to_bytes(4, "little")
     
     newFile = open("vivoFiles/" + text.split("\n")[i] + "_" + str(i + 1) + ".bin", "wb")
     newFile.write(bytE[int.from_bytes(oldOffset, "little"):int.from_bytes(newOffset, "little")])
--- FFC/Vivo Sizes and Ranges/splitVivosDescr.py
+++ FFC/Vivo Sizes and Ranges/splitVivosDescr.py
@@ -12,7 +12,8 @@ for i in range(210):
     oldOffset = bytE[(16 + (i * 4)):(20 + (i * 4))]
     newOffset = bytE[(20 + (i * 4)):(24 + (i * 4))]
     if (i == 209):
-        newOffset = len(bytE).to_bytes(4, "little")
+        newOffset = 25340
+        newOffset = newOffset.to_bytes(4, "little")
     
     newFile = open("vivoDescrFiles/" + text.split("\n")[i] + "_" + str(i + 1) + ".bin", "wb")
     newFile.write(bytE[int.from_bytes(oldOffset, "little"):int.from_bytes(newOffset, "little")])

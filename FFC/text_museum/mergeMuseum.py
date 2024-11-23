128cf2db19b826dad62d7bb349a1d452716909c2
opiter09 <92452568+opiter09@users.noreply.github.com>
2024-11-23 12:52:48 -0500

    size fixing

--- FFC/text_museum/mergeMuseum.py
+++ FFC/text_museum/mergeMuseum.py
@@ -21,4 +21,17 @@ if (os.path.exists("./museumFiles/") == True):
     binn.write((0x30950000).to_bytes(4, "big"))
     for i in range(210):
         binn.write(open("museumFiles/" + text.split("\n")[i] + "_" + str(i + 1) + ".bin", "rb").read())
+    binn.write(bytes(4))
+    binn.close()
+    
+    size = os.stat("0.bin").st_size
+    binn = open("0.bin", "rb")
+    r = binn.read()
+    binn.close()
+    binn = open("0.bin", "wb")
+    binn.close()
+    binn = open("0.bin", "ab")
+    binn.write(r[0:0x354])
+    binn.write((size - 4).to_bytes(4, "little"))
+    binn.write(r[0x358:])
     binn.close()
--- FFC/text_museum/splitMuseum.py
+++ FFC/text_museum/splitMuseum.py
@@ -12,7 +12,7 @@ for i in range(210):
     oldOffset = bytE[(12 + (i * 4)):(16 + (i * 4))]
     newOffset = bytE[(16 + (i * 4)):(20 + (i * 4))]
     if (i == 209):
-        newOffset = len(bytE).to_bytes(4, "little")
+        newOffset = (len(bytE) - 4).to_bytes(4, "little")
     
     newFile = open("museumFiles/" + text.split("\n")[i] + "_" + str(i + 1) + ".bin", "wb")
     newFile.write(bytE[int.from_bytes(oldOffset, "little"):int.from_bytes(newOffset, "little")])
